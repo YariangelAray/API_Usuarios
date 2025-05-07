dd9beab49fb9e2ee086bb329d001b43ed09c99ec
Yariangel Aray <gabrielamunar4@gmail.com>
2025-05-06 23:42:16 -0500

    Generos eliminar

--- Models/Usuario.js
+++ Models/Usuario.js
@@ -6,6 +6,11 @@ class Usuario{
         const [usuarios] = await connection.query("SELECT * FROM usuarios WHERE id_ciudad = ?", [idCiudad]);  
         return usuarios;
     }
+
+    async getByIdGenero(idGenero) {
+        const [usuarios] = await connection.query("SELECT * FROM usuarios WHERE id_genero = ?", [idGenero]);        
+        return usuarios;
+    }
 }
 
 export default Usuario;
